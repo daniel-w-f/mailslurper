daac1150be13953589e25edee3a8d2fe796ea555
daniel-w-f <daniel_wilms@gmx.de>
2022-10-29 15:55:55 +0200

    Initial template to send mails

---
+++ sendmail_ssl.py
@@ -0,0 +1,13 @@
+import smtplib, ssl
+
+# port = 465  # For SSL
+# Local Python DebuggingServer
+port = 1025
+password = input("Type your password and press enter: ")
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("my@gmail.com", password)
+    # TODO: Send email here
